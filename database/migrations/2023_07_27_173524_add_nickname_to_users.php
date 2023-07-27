eb54553448cb58cd22535fc3682eed55c25030ad
greneIT <jedwards@visionsinwebs.com>
2023-07-27 19:44:43 +0200

    Add column nickname
    Add the column nickname via migrations with length and unique requirements

---
+++ database/migrations/2023_07_27_173524_add_nickname_to_users.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            // Add the new column definition with its requirements
+			$table->string('nickname', 29)->unique();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            // Define rollback operation here
+			$table->dropColumn('nickname');
+        });
+    }
+};
