1b12ef65cddd5a3da0a9ba47ebeef51eb90a1816
greneIT <jedwards@visionsinwebs.com>
2023-07-28 10:42:39 +0200

    Add tests
    Add integration and acceptance tests

--- tests/Acceptance/UserControllerTest.php
+++ tests/Acceptance/UserControllerTest.php
@@ -28,6 +28,7 @@ class UserControllerTest extends FrameworkTest
                 'id'    => $user->id,
                 'name'  => $user->name,
                 'email' => $user->email,
+				'nickname' => $user->nickname,
             ],
             json_decode($result->getContent(), true)
         );
@@ -41,6 +42,7 @@ class UserControllerTest extends FrameworkTest
             'id'    => $user->id,
             'name'  => $this->faker->name,
             'email' => $user->email,
+			'nickname' => $user->nickname,
         ];
         $result = $this->put("/api/users/$user->id", $data);
         $result->assertSuccessful();
@@ -52,11 +54,32 @@ class UserControllerTest extends FrameworkTest
         $data = [
             'name'     => $this->faker->name,
             'email'    => $email = $this->faker->unique()->email,
+			'nickname' => $nickname = $this->faker()->unique()->userName(),
             'password' => 'hen rooster chicken duck',
         ];
         $this->assertFalse($this->repository->getModel()->newQuery()->where('email', $email)->exists());
+		$this->assertFalse($this->repository->getModel()->newQuery()->where('nickname', $nickname)->exists());
         $result = $this->post("/api/users", $data);
         $result->assertSuccessful();
         $this->assertTrue($this->repository->getModel()->newQuery()->where('email', $email)->exists());
+		$this->assertTrue($this->repository->getModel()->newQuery()->where('nickname', $nickname)->exists());
     }
+	
+	public function testCreateUserWithDuplicateNicknameWillNotWork()
+    {
+        $firstUser = User::factory()->create();
+
+        $data = [
+            'name'     => $this->faker->name,
+            'email'    => $this->faker()->unique()->email(),
+            'password' => 'hen rooster chicken duck',
+            'nickname' => $firstUser->nickname,
+        ];
+
+        $response = $this->post("/api/users", $data);
+		
+		// Assert that the request was not successful (HTTP status 302 as failedValidation has a redirect url).
+		$response->assertStatus(302);
+	}
+
 }
